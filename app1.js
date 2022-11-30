18d783b55957648dd7486cfd588122529fba8326
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2022-11-30 12:38:58 +0530

    first commit

--- app1.js
+++ app1.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,23 +15,20 @@ const shopRoutes = require('./routes/shop');
 const contactusRoutes = require('./routes/contactus');
 const successRoutes = require('./routes/success');
 
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactusRoutes);
 app.use(successRoutes);
 
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'views', 'success.html'));
-});
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'views', 'success.html'));
+// });
+app.use(errorController.get404);
 
 app.listen((8888),()=>{
-    console.log('on port 8888') 
+    console.log('on port 8888');
 });
